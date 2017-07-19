d7f5d2c518a9f1727de0ff3a07f87d46b427ce53
Alfonso Rodríguez <arodriguez@teltek.es>
2017-07-19 16:39:46 +0200

    space/emacs: Improved create-symlinks script

--- emacs/create-symlinks.sh
+++ emacs/create-symlinks.sh
@@ -1,12 +1,29 @@
-rm ~/.emacs
-ln -s ~/.c12n/emacs/.emacs ~/.emacs
+# Remove existing emacs files (asking for it)
+rm -ri ~/.emacs
+rm -ri ~/.emacs.d
+rm -ri ~/.spacemacs
 
-rm ~/.spacemacs
-ln -s ~/.c12n/emacs/.spacemacs ~/.spacemacs
+# Enable emacs/spacemacs: (Still didn't figure how to have both simultaneously)
+read -n1 -p "Install spacemacs?" doit
+case $doit in
+    y|Y)
+        ln -s ~/.c12n/emacs/.spacemacs ~/.spacemacs
+        ln -s ~/.c12n/emacs/.spacemacs.d ~/.emacs.d
+        ;;
+    *)
+        printf "\nSpacemacs not installed\n"
+        read -n1 -p "Install emacs?" doit
+        case $doit in
+            y|Y)
+                ln -s ~/.c12n/emacs/.emacs ~/.emacs
+                ln -s ~/.c12n/emacs/.emacs ~/.emacs.d
+                ;;
+            *)
+                printf "\nEmacs not installed.\n NOTHING installed!!"
+                ;;
+        esac
+        ;;
+esac
+printf "\n";
 
-rm ~/.emacs.d
-ln -s ~/.c12n/emacs/.emacs ~/.emacs.d
-
-rm ~/.spacemacs.d
-ln -s ~/.c12n/emacs/.spacemacs.d ~/.spacemacs.d
 
