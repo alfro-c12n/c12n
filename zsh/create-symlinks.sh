4eab523e02e517d62f133e37290d65feeff727f4
Alfonso Rodríguez <arodriguez@teltek.es>
2018-11-09 13:11:44 +0100

    Added i3 base config plus symlink

--- zsh/create-symlinks.sh
+++ zsh/create-symlinks.sh
@@ -6,3 +6,6 @@ ln -s ~/.c12n/zsh/.oh-my-zsh/ ~/.oh-my-zsh
 
 echo "Setting up zsh as default shell (chsh -s /bin/zsh $USER)"
 chsh -s /bin/zsh $USER
+
+rm -rf ~/.config/i3/config
+ln -s ~/.c12n/i3/config ~/.config/i3/config
