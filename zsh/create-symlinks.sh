f19ba75f9cc9ffe53313fc46288f664f15204755
Alfro <alfonsorguezp@gmail.com>
2018-11-14 20:29:34 +0100

    Fix issue with symlinks script

--- zsh/create-symlinks.sh
+++ zsh/create-symlinks.sh
@@ -1,8 +1,8 @@
 rm ~/.zshrc
 ln -s ~/.c12n/zsh/.zshrc ~/.zshrc
 
-rm -rf ~/.oh-my-zsh/
-ln -s ~/.c12n/zsh/.oh-my-zsh/ ~/.oh-my-zsh
+rm ~/.oh-my-zsh
+ln -s ~/.c12n/zsh/.oh-my-zsh ~/.oh-my-zsh
 
 echo "Setting up zsh as default shell (chsh -s /bin/zsh $USER)"
 chsh -s /bin/zsh $USER
